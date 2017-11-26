fcf43f55947a5e4c9b32c224bc1b91814c178663
Momiji Jin <Moeprprpr@gmail.com>
2017-11-26 10:17:12 +0800

    添加 AJAX 评论支持

--- lib/Const.php
+++ lib/Const.php
@@ -1,4 +1,8 @@
 <?php
 class THI_Const {
     const STATIC_URL = 'https://static.misaka.xin';
+    
+    const COMMENT_SYSTEM_EMBED = 0;
+    const COMMENT_SYSTEM_DISQUS = 1;
+    const COMMENT_SYSTEM_CHANGYAN = 2;
 }
