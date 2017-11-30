eb00bd6fd6764a2e7dcadd6a0f024d8fa22a08da
Momiji Jin <Moeprprpr@gmail.com>
2017-11-30 21:39:18 +0800

    修改外链图片地址

--- lib/Const.php
+++ lib/Const.php
@@ -1,6 +1,6 @@
 <?php
 class THI_Const {
-    const STATIC_URL = 'https://static.misaka.xin';
+    const STATIC_URL = 'https://public.misaka.xin';
     
     const COMMENT_SYSTEM_EMBED = 0;
     const COMMENT_SYSTEM_DISQUS = 1;
